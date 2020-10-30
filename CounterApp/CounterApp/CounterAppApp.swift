537ca2fe7772007598594171b4e0b5f0e142820d
Ossamoon <osamu950623@gmail.com>
2020-10-30 09:04:02 +0900

    本の内容に従って完成させた

---
+++ CounterApp/CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by 齋藤修 on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
